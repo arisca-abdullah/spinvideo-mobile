fb05653828da94b9eedac47c9be0fb67446d3e62
Arisca Abdullah <aris@nocola.co.id>
2023-03-17 07:55:44 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'remote-video-recorder',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
